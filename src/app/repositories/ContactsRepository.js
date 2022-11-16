c41ba4feac31da0b54aa8e32a5a50514bbcac250
zkauansantos <kauansantosdepontes@gmail.com>
2022-11-16 09:53:22 -0300

    method update refatored

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -45,22 +45,14 @@ class ContactsRepository {
      return row;
   }
 
-  update(id, { name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const contactUpdated = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-
-      contacts = contacts.map((contact) => (
-        contact.id === id ? contactUpdated : contact
-      ));
-
-      resolve(contactUpdated);
-    });
+  async update(id, { name, email, phone, category_id }) {
+    const [row] = await db.query(`
+    UPDATE contacts
+    SET name = $1, email = $2, phone = $3, category_id = $4
+    WHERE id = $5
+    RETURNING *
+    `, [name, email, phone, category_id, id]);
+    return row;
   }
 
   delete(id) {
